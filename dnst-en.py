f0f893b26ea8638d3abc4969e073b5b9779a1599
chengmoxu <rolly200311@gmail.com>
2024-07-01 15:42:11 +0800

    Added default gateway input mechanism

--- dnst-en.py
+++ dnst-en.py
@@ -1,28 +1,30 @@
-# Dorm Network Setting Tool for NCCU (English) v1.1
-# Build: v1.1.20231117.py.1
-# Python Version: 3.12
+# Dorm Network Setting Tool for NCCU (English) v2.0
+# Build: v2.0.20240701.py.1
+# Python Version: 3.12.2
 
-from os import *
+import os
+
 print("Dorm Network Setting Tool for NCCU (English) v1.1")
-ip = str(input("Please enter your IP address from NCCU Computer Center:"))
-link_name = str(input("Please enter your Internet name:"))
-ip_set_command = ('netsh interface ipv4 set address name="' + link_name + '" static '+ ip + ' 255.255.255.0 140.119.194.254')
+link_name = str (input ("Please enter your Internet name:"))
+ip = str (input ("Please enter your IP address from NCCU Computer Center:"))
+default_gateway = str (input ("Please enter the default gateway IP location from NCCU Computer Center:"))
+ip_set_command = ('netsh interface ipv4 set address name="' + link_name + '" static '+ ip + ' 255.255.255.0 ' + default_gateway)
 dns1_set_command = ('netsh interface ipv4 set dns name="' + link_name + '" static 140.119.1.110')
 dns2_set_command = ('netsh interface ipv4 add dns name="' + link_name + '" 140.119.252.12')
-ip_status = system(ip_set_command)
-dns1_status = system(dns1_set_command)
-dns2_status = system(dns2_set_command)
+ip_status = os.system (ip_set_command)
+dns1_status = os.system (dns1_set_command)
+dns2_status = os.system (dns2_set_command)
 
 if ip_status == 0:
-    print("IP setting successful")
+    print ("IP setting successful")
 else:
-    print("IP setting failed, please check whether the network card name or IP address is entered correctly")
+    print ("IP setting failed, please check whether the network card name or IP address is entered correctly")
 if dns1_status == 0:
-    print("DNS setting successful")
+    print ("DNS setting successful")
 else:
-    print("DNS setting failed, please check whether the network card name or IP address is entered correctly")
+    print ("DNS setting failed, please check whether the network card name or IP address is entered correctly")
 if dns2_status == 0:
-    print("Add DNS setting successful")
+    print ("Add DNS setting successful")
 else:
-    print("Add DNS setting failed, please check whether the network card name or IP address is entered correctly")
-system("Pause")
+    print ("Add DNS setting failed, please check whether the network card name or IP address is entered correctly")
+os.system ("Pause")
--- dnst.py
+++ dnst.py
@@ -1,29 +1,31 @@
-# NCCU宿舍網路設定工具 v1.1
-# Dorm Network Setting Tool for NCCU v1.1
-# Build: v1.1.20230702.py.1
-# Python Version: 3.11
+# NCCU宿舍網路設定工具 v2.0
+# Dorm Network Setting Tool for NCCU v2.0
+# Build: v2.0.20240701.py.1
+# Python Version: 3.12.2
 
-from os import *
-print("NCCU宿舍網路設定工具 v1.1")
-ip = str(input("請輸入您欲設定的ip位置:"))
-link_name = str(input("請輸入您的連線名稱:"))
-ip_set_command = ('netsh interface ipv4 set address name="' + link_name + '" static '+ ip + ' 255.255.255.0 140.119.194.254')
+import os
+
+print ("NCCU宿舍網路設定工具 v2.0")
+link_name = str (input ("請輸入您的連線名稱:"))
+ip = str (input ("請輸入您欲設定的IP位置:"))
+default_gateway = str (input ("請輸入系統顯示的預設閘道IP位置:"))
+ip_set_command = ('netsh interface ipv4 set address name="' + link_name + '" static '+ ip + ' 255.255.255.0 ' + default_gateway)
 dns1_set_command = ('netsh interface ipv4 set dns name="' + link_name + '" static 140.119.1.110')
 dns2_set_command = ('netsh interface ipv4 add dns name="' + link_name + '" 140.119.252.12')
-ip_status = system(ip_set_command)
-dns1_status = system(dns1_set_command)
-dns2_status = system(dns2_set_command)
+ip_status = os.system (ip_set_command)
+dns1_status = os.system (dns1_set_command)
+dns2_status = os.system (dns2_set_command)
 
 if ip_status == 0:
-    print("IP設定成功")
+    print ("IP設定成功")
 else:
-    print("IP設定失敗，請檢查網卡名稱或IP位置是否輸入正確")
+    print ("IP設定失敗，請檢查網卡名稱或IP位置是否輸入正確")
 if dns1_status == 0:
-    print("慣用DNS伺服器設定成功")
+    print ("慣用DNS伺服器設定成功")
 else:
-    print("慣用DNS伺服器設定失敗，請檢查網卡名稱或IP位置是否輸入正確")
+    print ("慣用DNS伺服器設定失敗，請檢查網卡名稱或IP位置是否輸入正確")
 if dns2_status == 0:
-    print("其他DNS伺服器設定成功")
+    print ("其他DNS伺服器設定成功")
 else:
-    print("其他DNS伺服器設定失敗，請檢查網卡名稱或IP位置是否輸入正確")
-system("Pause")
+    print ("其他DNS伺服器設定失敗，請檢查網卡名稱或IP位置是否輸入正確")
+os.system ("Pause")
